c59ce6b01c3040333fc9521d732dfa912f773f85
eduardo cannini thome <eduardocanninithome@hotmail.com>
2017-03-13 19:08:26 -0300

    add back top button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,25 @@ $(document).ready(function(){
 
 	"use strict"; // Start of use strict
 
+	$(window).scroll(function () {
+        if ($(this).scrollTop() > 20) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+
+
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
@@ -66,19 +85,4 @@ $(document).ready(function(){
         distance: '0px'
     }, 300);
 
-    // Initialize and Configure Magnific Popup Lightbox Plugin
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
 });
